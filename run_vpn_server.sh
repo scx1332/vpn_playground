72650bf88fdd665050a0e97a76d53a79f025599c
scx1332 <sieciech.czajka@golem.network>
2023-03-06 16:12:25 +0100

    f

--- run_vpn_server.sh
+++ run_vpn_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting VPN server"
 cd novpn-c
-./novpn 45918
+./novpn server 45918
 wait_sec=10000000
 echo "Waiting for $wait_sec seconds before leaving the container..."
 sleep $wait_sec
